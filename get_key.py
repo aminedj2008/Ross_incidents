ed761aba50ef65dcf535620a9d117de31454cdf4
amine djenidi <73023204+aminedj2008@users.noreply.github.com>
2024-01-20 04:09:44 +0100

    Update get_key.py

--- get_key.py
+++ get_key.py
@@ -1,16 +1,1 @@
-import pickle
-from pathlib import Path
 
-import streamlit_authenticator as stauth 
-
-
-
-names =["SPS","MSPS"]
-usernames = ["SPS","MSPS"]
-passwords = ["xxxxxx","xxxxxxx"]
-
-hash_password = stauth.Hasher(passwords).generate()
-
-file_p=Path(__file__).parent/"password_key.pk1"
-with file_p.open("wb") as file :
-    pickle.dump(hash_password, file)
